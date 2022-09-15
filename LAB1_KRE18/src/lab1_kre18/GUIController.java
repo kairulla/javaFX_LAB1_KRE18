d785669052d0e7d772434ca7becf1d63588ff3d2
kairulla <kayrulla@bk.ru>
2022-09-15 09:26:45 +0600

    first commit

---
+++ LAB1_KRE18/src/lab1_kre18/GUIController.java
@@ -0,0 +1,72 @@
+package lab1_kre18;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GUIController implements Initializable {
+
+    @FXML
+    private TextField textFieldA;
+    @FXML
+    private TextField textFieldB;
+    @FXML
+    private TextField textFieldX;
+    @FXML
+    private Button buttonSolve;
+    @FXML
+    private Button buttonClean;
+    @FXML
+    private Button buttonExit;
+    @FXML
+    private Label labelOtvet;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+
+    @FXML
+    private void buttonSolveAction(ActionEvent event) {
+        double a = 0, b = 0, x = 0, y = 0;
+        try {
+            a = Double.parseDouble(textFieldA.getText());
+            b = Double.parseDouble(textFieldB.getText());
+            x = Double.parseDouble(textFieldX.getText());          
+        } catch (Exception ex) {
+            buttonCleanAction(null);
+            textFieldA.requestFocus();
+//            ex.printStackTrace();
+        }
+        
+        if (x > 6) {
+            y = (6 * x * x - a * b) / (2 * x * x);
+        } else {
+            y = 4 * (x + a * a + b * b);
+        }
+        
+        if (!(Double.isNaN(y)) && (!Double.isInfinite(y))) {
+            labelOtvet.setText("Ответ: " + String.format("%.2f", y));
+        } else {
+            labelOtvet.setText("Нет решения!");
+        }
+    }
+
+    @FXML
+    private void buttonCleanAction(ActionEvent event) {
+        labelOtvet.setText("Ответ: ");
+        textFieldA.setText("");
+        textFieldB.setText("");
+        textFieldX.setText("");
+    }
+
+    @FXML
+    private void buttonExitAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+}
---
+++ LAB1_KRE18/src/lab1_kre18/MainClass.java
@@ -0,0 +1,26 @@
+package lab1_kre18;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Лабораторная 1");
+        stage.getIcons().add(new Image(MainClass.class.getResourceAsStream("logo.png")));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
